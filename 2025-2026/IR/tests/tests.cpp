73fae2236e3a4262b30b8cf84ac4a79efaad4d78
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2025-09-28 21:29:32 +0200

    Update tests.cpp

--- 2025-2026/IR/tests/tests.cpp
+++ 2025-2026/IR/tests/tests.cpp
@@ -1,1 +1,38 @@
+// #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
 
+#define NB_TOURS 5
+
+int main()
+{
+	/*
+	int i;
+	for (i = 0; i < NB_TOURS; i++)
+	{
+		printf("Bonjour\n");
+	}
+
+	int i;
+	int j;
+	for (i = 0; i < NB_TOURS; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			printf("\t");
+		}
+		printf("Bonjour\n");
+	}
+	*/
+
+	int i;
+	int j;
+	for (i = NB_TOURS; i > 0; i--)
+	{
+		for (j = 1; j < i; j++)
+		{
+			printf("\t");
+		}
+		printf("Bonjour\n");
+	}
+}

8cffda08cdbb8450bcff8c9c87f57a6b2cddefaa
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2025-09-29 12:15:04 +0000

    new file:   2025-2026/IR/tests/for.cpp 	modified:   2025-2026/IR/tests/tests 	deleted:    2025-2026/IR/tests/tests.cpp 	new file:   2025-2026/IR/tests/while.cpp

---
+++ 2025-2026/IR/tests/for.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NB_TOURS 5
+int main() 
+{
+	// V1
+	for (int i = 0; i < NB_TOURS; i = i + 1) {
+		printf("Bonjour\n");
+	}
+
+	// V2
+	for (int i = 0; i < NB_TOURS; i = i + 1) {
+	// Decalage vertical
+
+		for (int j = 0; j < i; j = j + 1) {
+		// Decalage horizontal
+			// i decalages
+			printf("\t");
+		}
+		printf("Bonjour\n");
+	}
+
+	// V3
+	for (int i = 0; i < NB_TOURS; i = i + 1) {
+
+		for (int j = NB_TOURS - i - 1; 0 < j; j = j - 1) {
+			printf("\t");
+		}
+		printf("Bonjour\n");
+	}
+}

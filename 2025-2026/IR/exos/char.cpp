961bb6c59ec2e38063dd86e7a270b427f28242aa
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2025-11-03 09:54:21 +0000

    new file:   2025-2026/IR/exos/char.cpp

---
+++ 2025-2026/IR/exos/char.cpp
@@ -0,0 +1,20 @@
+//       Tableau de char, 2 options :
+// -----------------------------------------
+//
+//     1. Parcourir tout le tableau
+//         Taille connue => Boucle FOR
+//
+//     2. TabChar contient une chaine
+//         => Chercher fin de chaine '\0'
+//         Boucle WHILE
+//
+// -----------------------------------------
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+}

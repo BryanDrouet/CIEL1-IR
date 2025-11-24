03819141bbc294c2fb611aeeeacbf06c3a9c38f5
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2025-11-24 14:54:21 +0100

    Update sousProg+Return.cpp

--- 2025-2026/IR/tests/sousProg+Return.cpp
+++ 2025-2026/IR/tests/sousProg+Return.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addV1(int x, int y) {
-    return (x + y);
-}
+int addV1(int x, int y);
+int addV2(int* px, int* py);
+void addV3(int* px, int* py, int* pz);
 
 int main()
 {
@@ -12,5 +12,22 @@ int main()
     int Z;
 
     Z = addV1(X, Y); // Z <= X + Y
-    printf("%d", Z);
+    printf("%d \n", Z);
+
+    Z = addV2(&X, &Y);
+    printf("%d \n", Z);
+
+    addV3(&X, &Y, &Z);
+}
+
+int addV1(int x, int y) {
+    return (x + y);
+}
+
+int addV2(int* px, int* py) {
+    return (*px + *py);
+}
+
+void addV3(int* px, int* py, int* pz) {
+    *pz = *px + *py;
 }

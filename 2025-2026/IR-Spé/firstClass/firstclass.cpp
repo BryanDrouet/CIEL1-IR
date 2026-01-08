a5f7dc7d3a119131471a23514e64c60765cde2d3
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2026-01-08 11:21:30 +0100

    Update main function to use Point structure

--- 2025-2026/IR-Spé/firstClass/firstclass.cpp
+++ 2025-2026/IR-Spé/firstClass/firstclass.cpp
@@ -1,7 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "Point.h"
 
 int main()
 {
-    initPoint();
-    afficherPoint();
+    Point P1;
+    initPoint(&P1, 10, 20);
+    afficherPoint(&P1);
 }
